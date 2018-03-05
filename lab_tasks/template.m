f9830b3461d414d1635a579b9f29c19ed453f709
Aadam <aadimator@gmail.com>
2018-03-05 18:42:40 +0500

    Add template for lab tasks

---
+++ lab_tasks/template.m
@@ -0,0 +1,15 @@
+%{
+Digit Image Processing
+
+Name: Aadam
+Roll# 6396
+Program: BCS 8th
+Semester: Spring 2018
+Department of IT
+Universiy of Haripur
+
+Lab Task# n
+Basic Matrix operations in MATLAB
+Displaying a bar plot
+%}
+
