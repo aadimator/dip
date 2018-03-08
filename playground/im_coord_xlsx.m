e1ef3389d5147d0105eaf9ec981d17aa3ecc45a9
Aadam <aadimator@gmail.com>
2018-03-08 19:09:03 +0500

    Save coords in an excel file

--- playground/im_coord_xlsx.m
+++ playground/im_coord_xlsx.m
@@ -3,25 +3,34 @@ Digital Image Processing
 
 Name: Aadam
 
-Save the co-ordinates of a specific pixel in an image into an
-excel file.
+Save the co-ordinates of pixels in a specific range in an image 
+into an excel file.
 
 %}
 
 clearvars; close all; clc;
 
 im = imread('../images/Baby_Po.jpg');
-% imshow(im);
-[r, c, d] = size(im);
-mask = ones(r, c);
 
-for i=1:r
-   for j = 1:c
-      if im(i, j, :) > 150
-         mask(i, j) = 0; 
-      end
-   end
+[rows, cols, channels] = size(im);
+
+% create a mask
+mask = zeros(rows, cols);
+count = 0;
+
+for i=1:rows
+    for j = 1:cols
+        % if the pixels are less than 028
+        if im(i, j, :) < 028
+            mask(i, j) = 1; % replace zero (black) with one (white)
+            count = count + 1;
+            coords(count, 1) = i;
+            coords(count, 2) = j;
+        end
+    end
 end
 
 subplot(1, 2, 1); imshow(im);
 subplot(1, 2, 2); imshow(mask);
+
+xlswrite('coord.xlsx', coords);
