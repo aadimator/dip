2621ec1fe0a9ef7648fc7502d2ed4cff994d9179
Aadam <aadimator@gmail.com>
2018-03-24 10:50:54 +0500

    initial lab6

---
+++ lab_tasks/lab6_6396.m
@@ -0,0 +1,16 @@
+%{
+Digital Image Processing
+
+Name: Aadam
+Roll# 6396
+Program: BCS 8th
+Semester: Spring 2018
+Department of IT
+Universiy of Haripur
+
+Lab: 
+
+
+%}
+
+clearvars; close all; clc;
