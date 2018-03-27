d84b91a0c60a48eed4cbabab44db2ff54bffdf7d
Aadam <aadimator@gmail.com>
2018-03-27 11:22:06 +0500

    feat: complete task1 lab 6

--- lab_tasks/lab6_6396.m
+++ lab_tasks/lab6_6396.m
@@ -10,7 +10,49 @@ Universiy of Haripur
 
 Lab: 6
 
+RGB boxes on white background
 
 %}
 
 clearvars; close all; clc;
+
+m = ones(400, 400, 3);
+
+size = [50 50];
+
+
+red_start = [50 50];
+red_end = red_start + size;
+
+for i = red_start(1):red_end(1)
+    for j = red_start(2):red_end(2)
+        m(i, j, 2:3) = 0;
+    end
+end
+   
+green_start = [50 300];
+green_end = green_start + size;
+
+for i = green_start(1):green_end(1)
+    for j = green_start(2):green_end(2)
+        m(i, j, 1) = 0;
+        m(i, j, 3) = 0;
+    end
+end
+
+blue_start = [300 50];
+blue_end = blue_start + size;
+
+for i = blue_start(1):blue_end(1)
+    for j = blue_start(2):blue_end(2)
+        m(i, j, 1:2) = 0;
+    end
+end
+
+imshow(m)
+
+
+
+
+
+
