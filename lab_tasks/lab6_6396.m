1aa3a6f656b328424c185be3db47d69f9e9c6f88
Aadam <aadimator@gmail.com>
2018-03-27 12:20:30 +0500

    feat: circle shown

--- lab_tasks/lab6_6396.m
+++ lab_tasks/lab6_6396.m
@@ -24,9 +24,16 @@ size = [50 50];
 red_start = [50 50];
 red_end = red_start + size;
 
+x0 = 75;
+y0 = 75;
+r = 25;
+
 for i = red_start(1):red_end(1)
     for j = red_start(2):red_end(2)
-        m(i, j, 2:3) = 0;
+%         m(i, j, 2:3) = 0;
+        if ((i-x0)^2 + (j-y0)^2 < r^2)
+            m(i, j, 2:3) = 0;
+        end
     end
 end
    
@@ -63,5 +70,9 @@ imshow(m)
 
 
 
+% xc = 5;
+% yc = 7;
+% r = 3;
+% circle(xc, yc, r);
 
 
