1be7c3ebcd7144fa6e95f6cc90e44ce3b72f93c3
Aadam <aadimator@gmail.com>
2018-03-29 21:55:23 +0500

    feat: add assignment template

---
+++ assignments/template.m
@@ -0,0 +1,15 @@
+%{
+Digital Image Processing
+
+Name: Aadam
+Roll# 6396
+Program: BCS 8th
+Semester: Spring 2018
+Department of IT
+Universiy of Haripur
+
+Assignment: 
+
+%}
+
+clearvars; close all; clc;
