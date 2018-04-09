f4b8dca359eee87c42b88c8813ce192b69159ed4
Aadam <aadimator@gmail.com>
2018-04-09 11:38:16 +0500

    work on lab 8

--- lab_tasks/DIP_Lab8.m
+++ lab_tasks/DIP_Lab8.m
@@ -5,22 +5,22 @@
 clear all; close all; clc;
 % %%
 % im = imread('cameraman.tif');
-% % imshow(im);
-% im1 = im - 50;
+% imshow(im);
+% im1 = im + 50;
 % figure;subplot(1,2,1);imhist(im);
 % subplot(1,2,2);imhist(im1);
 % %%
 % im = imread('cameraman.tif');
-% im1 = im - 50;
+% im1 = im + 50;
 % subplot(2,2,1);imshow(im);title('original image');
-% subplot(2,2,2);imshow(im1);title('original image - 50');
+% subplot(2,2,2);imshow(im1);title('original image + 50');
 % subplot(2,2,3);imhist(im);
 % subplot(2,2,4);imhist(im1);
 %%
 % im = imread('cameraman.tif');
-% im1 = im + 100;
+% im1 = im - 100;
 % subplot(2,2,1);imshow(im);title('original image');
-% subplot(2,2,2);imshow(im1);title('original image + 100');
+% subplot(2,2,2);imshow(im1);title('original image - 100');
 % subplot(2,2,3);imhist(im);
 % subplot(2,2,4);imhist(im1);
 
@@ -42,12 +42,12 @@ clear all; close all; clc;
 %% Contrast Adjustment
 %%
 %% Example-1
-% im = imread('cameraman.tif');
-% im1 = imadjust(im);
-% subplot(2,2,1);imshow(im);
-% subplot(2,2,2);imshow(im1);
-% subplot(2,2,3);imhist(im);
-% subplot(2,2,4);imhist(im1);
+im = imread('cameraman.tif');
+im1 = imadjust(im);
+subplot(2,2,1);imshow(im);
+subplot(2,2,2);imshow(im1);
+subplot(2,2,3);imhist(im);
+subplot(2,2,4);imhist(im1);
 %%
 %% Example-2a
 % im = imread('cameraman.tif');
@@ -78,13 +78,13 @@ clear all; close all; clc;
 % subplot(2,2,4);imhist(im1);
 %%
 %% Histogram matching
-im = imread('cameraman.tif');
-im = im-100;
-im1 = imadjust(im);
-subplot(2,2,1);imshow(im);
-subplot(2,2,2);imshow(im1);
-im2 = imhistmatch(im,im1);
-subplot(2,2,3);imshow(im2);
+% im = imread('cameraman.tif');
+% im = im-100;
+% im1 = imadjust(im);
+% subplot(2,2,1);imshow(im);
+% subplot(2,2,2);imshow(im1);
+% im2 = imhistmatch(im,im1);
+% subplot(2,2,3);imshow(im2);
 %%
 
 
