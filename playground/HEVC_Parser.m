0038e1c1359cf7b01b04472379b17fcf0b83220d
Aadam <aadimator@gmail.com>
2018-04-20 16:37:28 +0500

    feat: include YUV-PSNR in HEVC output:

--- playground/HEVC_Parser.m
+++ playground/HEVC_Parser.m
@@ -47,9 +47,10 @@ celldisp(time_cell);
 %% Extract Symmary data from the file
 frewind(fid);
 
-% Data format: 	       16    a     870.6600   34.2720   35.0217   36.1228   33.7482
+% Data format: 	       Total Frames |   Bitrate     Y-PSNR    U-PSNR    V-PSNR    YUV-PSNR
+%                             16    a     870.6600   34.2720   35.0217   36.1228   33.7482
 % Data is 2 lines ahead of "summary_line"
-summary_cell = textscan(fid,'%f %s %f %f',1,'delimiter', ' ', 'MultipleDelimsAsOne', 1, 'headerlines',summary_line + 1);
+summary_cell = textscan(fid,'%f %s %f %f %f %f %f',1,'delimiter', ' ', 'MultipleDelimsAsOne', 1, 'headerlines',summary_line + 1);
 
 celldisp(summary_cell);
 
@@ -60,6 +61,7 @@ fclose(fid);
 data(1,1) = time_cell{3};       % Total Time
 data(1,2) = summary_cell{3};    % Bitrate
 data(1,3) = summary_cell{4};    % Y-PSNR
+data(1,4) = summary_cell{7};    % YUV-PSNR
 
 xlswrite('HEVC_output.xlsx', data);
 
